8cd282b9436b6819d1d1a26bacd0640104d52f1a
eldrwy3-collab <eldrwy3@gmail.com>
2026-08-28 21:19:36 +0100

    Add files via upload

---
+++ app/security.js
@@ -0,0 +1,1 @@
+(()=>{document.addEventListener('keydown',e=>{if(e.key==='F12'||(e.ctrlKey&&e.shiftKey&&['I','J','C'].includes(e.key.toUpperCase())))e.preventDefault()});document.addEventListener('contextmenu',e=>e.preventDefault())})();
