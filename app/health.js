0f4060bea44c5f93bfa7e303cdbfa4a9ff5ff3c8
eldrwy3-collab <eldrwy3@gmail.com>
2026-08-28 13:54:18 +0100

    Delete app/health.js

--- app/health.js
+++
@@ -1,2 +0,0 @@
-'use strict';
-module.exports=async(req,res)=>{res.setHeader('Cache-Control','no-store');res.status(200).json({ok:true,service:'netregent',engine:'local-retrieval-composer',n8n:false});};
